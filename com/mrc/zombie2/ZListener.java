8f2a58eb45ef7f6ec324db3d3abb80bedcd7b9c2
Wes Hinsley <wes.hinsley@gmail.com>
2022-05-16 18:51:48 +0100

    Listen for run/delete signals from android

--- com/mrc/zombie2/ZListener.java
+++ com/mrc/zombie2/ZListener.java
@@ -2,6 +2,8 @@ package com.mrc.zombie2;
 
 import java.util.HashMap;
 
+import com.mrc.GKit.GList;
+import com.mrc.GKit.GListHeader;
 import com.mrc.GKit.GTextEntry;
 import com.mrc.GKit.GWebListener;
 
@@ -19,9 +21,16 @@ public class ZListener implements GWebListener {
     parent.ZG.gw.checkRepaint();
   }
 
+  private void updateSelector(GList gl, GListHeader glh, int index) {
+    gl.setSelected(index);
+    glh.paintOn(parent.ZG.gw.bi(), parent.ZG.gw.g2d());
+    parent.ZG.gw.requestRepaint();
+    parent.ZG.gw.checkRepaint();
+  }
+
   @Override
-  public void receiveMessage(HashMap<String, String> args) {
-    String cmd = args.get("cmd");
+  public String receiveMessage(HashMap<String, String> args) {
+   String cmd = args.get("cmd");
     if (cmd.equals("set")) {
       String param = args.get("param");
       String value = args.get("value");
@@ -29,20 +38,22 @@ public class ZListener implements GWebListener {
       else if (param.equals("Tinf")) updateText(parent.ZG.h_inf, value);
       else if (param.equals("vaccpc")) updateText(parent.ZG.i_p, value);
       else if (param.equals("vaccrad")) updateText(parent.ZG.i_rad, value);
-      else if (param.equals("vacccity")) parent.ZG.l_cities.setSelected(Integer.parseInt(value));
+      else if (param.equals("vacccity")) updateSelector(parent.ZG.l_cities, parent.ZG.lh_cities, Integer.parseInt(value));
       else if (param.equals("seeds")) updateText(parent.ZG.n_seeds, value);
       else if (param.equals("seedrad")) updateText(parent.ZG.n_seedradius, value);
-      else if (param.equals("seedcity")) parent.ZG.l_pcities.setSelected(Integer.parseInt(value));
+      else if (param.equals("seedcity")) updateSelector(parent.ZG.l_pcities, parent.ZG.lh_pcities, Integer.parseInt(value));
       else System.out.println("Error - param "+param+" not known in SET");
-    } else if (cmd.equals("T0")) parent.ZG.updateColSelect(parent.ZG.gt_col0);
-      else if (cmd.equals("T1")) parent.ZG.updateColSelect(parent.ZG.gt_col1);
-      else if (cmd.equals("T2")) parent.ZG.updateColSelect(parent.ZG.gt_col2);
-      else if (cmd.equals("T3")) parent.ZG.updateColSelect(parent.ZG.gt_col3);
-      else if (cmd.equals("D0")) parent.ZG.clearCol(parent.ZG.gt_col0);
-      else if (cmd.equals("D1")) parent.ZG.clearCol(parent.ZG.gt_col1);
-      else if (cmd.equals("D2")) parent.ZG.clearCol(parent.ZG.gt_col2);
-      else if (cmd.equals("D3")) parent.ZG.clearCol(parent.ZG.gt_col3);
-      else if (cmd.equals("GO")) parent.runSim();
-      else System.out.println("Unknown command "+cmd);
+      return "OK";
+
+    } else if (cmd.equals("R0")) { parent.ZG.updateColSelect(parent.ZG.gt_col0); parent.runSim(); return "WAIT"; }
+      else if (cmd.equals("R1")) { parent.ZG.updateColSelect(parent.ZG.gt_col1);  parent.runSim(); return "WAIT"; }
+      else if (cmd.equals("R2")) { parent.ZG.updateColSelect(parent.ZG.gt_col2);  parent.runSim(); return "WAIT"; }
+      else if (cmd.equals("R3")) { parent.ZG.updateColSelect(parent.ZG.gt_col3);  parent.runSim(); return "WAIT"; }
+      else if (cmd.equals("D0")) { parent.ZG.clearCol(parent.ZG.gb_ecol0); return "STOP_WAITING"; }
+      else if (cmd.equals("D1")) { parent.ZG.clearCol(parent.ZG.gb_ecol1); return "STOP_WAITING"; }
+      else if (cmd.equals("D2")) { parent.ZG.clearCol(parent.ZG.gb_ecol2); return "STOP_WAITING"; }
+      else if (cmd.equals("D3")) { parent.ZG.clearCol(parent.ZG.gb_ecol3); return "STOP_WAITING"; }
+      else if (cmd.equals("BUSY")) return String.valueOf(parent.ZG.gw.locked ? "WAIT" : "STOP_WAITING");
+    return "OK";
   }
 }
